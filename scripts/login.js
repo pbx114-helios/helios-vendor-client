48e7946365be237b5872d0962c6e8121117c443e
TanmeenPatel <tanmeenpatel@gmail.com>
2023-03-31 20:49:25 +0400

    initial commit, login complete

---
+++ scripts/login.js
@@ -0,0 +1,10 @@
+function submit() {
+    let name = document.getElementById("username").value
+    let pwd = document.getElementById("password").value
+    if (true) {
+        window.location.href = "./home.html"
+    }
+    else {
+        localStorage.check = value
+    }
+}
